44a9742191c9f40732861a16f0caac7e4f30e8db
SemSnel <s.snel1@student.avans.nl>
2021-03-27 18:07:21 +0100

    add library

---
+++ TeaAndChocolateLibrary/HotChocolate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeaAndChocolateLibrary
+{
+    public class HotChocolate
+    {
+        private bool _isDeluxe;
+
+        public HotChocolate()
+        {
+            _isDeluxe = false;
+        }
+
+        public string GetNameOfDrink()
+        {
+            return "HotChocolate";
+        }
+
+        public IEnumerable<string> GetBuildSteps()
+        {
+            var steps = new List<string>();
+
+            return steps;
+        }
+
+        public double Cost()
+        {
+            return 1.0;
+        }
+
+        public void MakeDeluxe()
+        {
+            _isDeluxe = true;
+        }
+    }
+}
---
+++ TeaAndChocolateLibrary/TeaBlend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TeaAndChocolateLibrary
+{
+    public class TeaBlend
+    {
+        public Color BagColor { get; set; }
+
+        public string Name { get; set; }
+    }
+}
